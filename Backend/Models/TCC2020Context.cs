3278d379786cc0662530c92dca6bca7f932127a2
Igor <Igorrbr2004@gmail.com>
2020-10-05 10:28:58 -0300

    TCC2020

---
+++ Backend/Models/TCC2020Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Backend.Models
+{
+    public partial class TCC2020Context : DbContext
+    {
+        public TCC2020Context()
+        {
+        }
+
+        public TCC2020Context(DbContextOptions<TCC2020Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;user id=root;password=1234;database=TCC2020", x => x.ServerVersion("8.0.18-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
